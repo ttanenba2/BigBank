299c3943a3a8a8eaf1903c58f0dae1e55bb24bc7
ttanenba2 <44252272+ttanenba2@users.noreply.github.com>
2019-01-06 15:52:07 -0500

    Update Transaction.java

--- src/ProgrammingAssignment/Transaction.java
+++ src/ProgrammingAssignment/Transaction.java
@@ -5,7 +5,7 @@ import java.time.*;
 import java.util.Scanner;
 
 public class Transaction {
-	private long lastTransactionID;
+	private static long lastTransactionID=0;
 	private String accountID;
 	private double transAmount;
 	private LocalDate transDate;
@@ -13,19 +13,38 @@ public class Transaction {
 	private TransType transType;
 	private Scanner scan;
 	
-	public Transaction() {
-		
-		//get transid form file
-		File file=new File("c:/transactionIDs.txt");//I couldnt save files ther
-		
-		try {
-			scan = new Scanner(file);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		lastTransactionID=scan.nextLong();
+	public Transaction(){
+		initializeTransactionID(C:\transactionID.txt);
+		saveLastTransactionID(C:\transactionID.txt);
 	}
+	
+	
+	private void initializeTransactionID (string filename) throws IOException {
+		try{
+			File transIdFile=new File(filename);
+		Scanner input=new Scanner(transIdFile);//open input file in a readable format
+		lastTransactionID=input.nextInt();
+	    this.transID=lasttransID++;
+	}
+catch (FileNotFoundException e){
+	System.out.println("We cannot find that file. Please try again.");
+}
+	}
+	
+	
+	private  void saveLastTransactionID(String filename) throws IOException{
+		try{
+			Integer chadash=lastTransactionID;
+			FileWriter filewriter= new FileWriter(filename);
+			filewriter.write(chadash);
+			filewriter.close();
+		}
+	catch (FileNotFoundException	e){
+		System.out.println("We cannot find that file. Please try again.");
+	}
+	}
+	
+	
 	
 	public String getAccountID() {
 		return accountID;
